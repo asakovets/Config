0666a7381a67c875acf6e2162fe8ea9e0e3ff228
Aleksey Sakovets <alexeysakovets@gmail.com>
2025-12-26 21:57:24 +0300

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if type --query mise
+    mise activate fish | source
+end
+
+set -x EDITOR nvim
+
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../../'
+
+alias	o "xdg-open"
+alias	cls "clear"
+
+abbr --add --position anywhere L '| less'
+abbr --add re-ne --set-cursor 'readelf --all % | rg NEEDED'
+abbr --add re-rp --set-cursor 'readelf --all % | rg "RUNPATH|RPATH"'
+
+alias ob objdump
+alias re readelf
+alias ni ninja
+
+alias +x 'chmod +x'
+alias psg 'ps aux | rg'
+
+abbr py python
+
+if test -f ~/.config/fish/local.fish
+    source ~/.config/fish/local.fish
+end
+
